d2780f6ab5fc833c6dc77193a7831fe508d613a5
Ria-Lal <rialal039@gmail.com>
2023-05-26 12:37:06 +0530

    Removed a newly created test file

--- src/test/java/com/mesgs/springlistener/SpringlistenerApplicationTests.java
+++ src/test/java/com/mesgs/springlistener/SpringlistenerApplicationTests.java
@@ -1,13 +1,19 @@
 package com.mesgs.springlistener;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.javamail.JavaMailSender;
+
 import com.mesgs.springlistener.controller.ListenerController;
+import com.mesgs.springlistener.entity.EmailDetails;
 import com.mesgs.springlistener.repository.MessagingRepo;
 import com.mesgs.springlistener.service.EmailService;
+import com.mesgs.springlistener.serviceImpl.EmailServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 class SpringlistenerApplicationTests {
@@ -20,6 +26,19 @@ class SpringlistenerApplicationTests {
 	
 	@Mock
 	EmailService emailService;
+	
+	@InjectMocks
+	EmailServiceImpl emailServiceImpl;
+	
+	@Mock
+	JavaMailSender javaMailSender;
+	
+	@Test
+	void sendSimpleEmailTest() {
+		String message = "Hi";
+		assertEquals("Mail Sent Successfully...", 
+			emailServiceImpl.sendSimpleMail(new EmailDetails("snehamayees@kpmg.com",message,"Message Notification from Azure Service Bus")));
+	}
 	
 	@Test
 	void receiveMessageTest() {
